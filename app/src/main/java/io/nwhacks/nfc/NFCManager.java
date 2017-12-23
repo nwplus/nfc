288d268f295e4c62e257fcfca2b317799207f8f0
CK <chamkank@users.noreply.github.com>
2017-12-22 17:31:25 -0800

    add read NFC tag methods
    I made it return an ArrayList of records as opposed to a single String of concatenated records (just in case we want to store more than one record on each tag)

--- app/src/main/java/io/nwhacks/nfc/NFCManager.java
+++ app/src/main/java/io/nwhacks/nfc/NFCManager.java
@@ -1,7 +1,15 @@
 package io.nwhacks.nfc;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.nfc.NfcAdapter;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.tech.Ndef;
+import android.nfc.Tag;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by rice on 11/30/17.
@@ -19,5 +27,34 @@ public class NFCManager {
         nfcAdpt = NfcAdapter.getDefaultAdapter(activity);
 
         return nfcAdpt != null;
+    }
+
+    public ArrayList<String> readTagFromIntent(Intent intent){
+        Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+        return readTag(tag);
+    }
+
+    /* Return all text records from NFC tag.*/
+    public ArrayList<String> readTag(Tag tag){
+        ArrayList<String> contents = new ArrayList<String>();
+        Ndef ndefTag = Ndef.get(tag);
+
+        NdefMessage ndefMesg = ndefTag.getCachedNdefMessage();
+        if (ndefMesg == null) {
+            return contents;
+        }
+        NdefRecord[] ndefRecords = ndefMesg.getRecords();
+        if (ndefRecords == null){
+            return contents;
+        }
+
+        for (NdefRecord record : ndefRecords){
+            String type = new String(record.getType());
+            if (Arrays.equals(type.getBytes(), NdefRecord.RTD_TEXT)){
+                String recordContents = new String(record.getPayload());
+                contents.add(recordContents);
+            }
+        }
+        return contents;
     }
 }
